737ac16491d9c40242ed02b8b481a539012d351f
asakumolemon <asakumolemon@gmail.com>
2025-03-14 15:44:18 +0800

    增加app.py注释 提高可读性

--- python/app.py
+++ python/app.py
@@ -9,6 +9,8 @@ import numpy as np
 import json
 import redis
 
+    # Jaccard系数 计算两个动画的粉丝重合度，得出动画的可能相似度
+    #
 def anime_Jaccard(df,anime_x,anime_y):
     anime1_users = set(df[df['anime_id'] == anime_x]['user_id'])
     anime2_users = set(df[df['anime_id'] == anime_y]['user_id'])
@@ -16,6 +18,7 @@ def anime_Jaccard(df,anime_x,anime_y):
     union = len(anime1_users.union(anime2_users))
     return intersection / union
 
+    # 计算两个动画的评分差值，得出用户对于动画的评分偏好
 def anime_rating_deficiency(df,anime_x,anime_y):
     book1_ratings = df[df['anime_id'] == anime_x]
     book2_ratings = df[df['anime_id'] == anime_y]
@@ -38,6 +41,11 @@ def getSql(id):
     list = ['user_id','anime_id','rating']
     return pd.DataFrame(results,columns=list)
 
+    # 通过Jaccard系数计算用户评分过后的动画与其他相似用户的喜欢的动画的相似度 Jaccard系数越高 说明两个作品的粉丝重合度越高 越有可能符合口味
+    # some_anime_id 用户评分过的动画id
+    # some_result 相似用户喜欢的动画
+    # df 用户评分矩阵
+    # 返回值为一个列表 包含了相似度最高的动画id 按照相似度降序排列
 def rcm_result_protol(some_anime_id, some_result, df):
     def calculate_similarity(anime_id, result):
         return {
@@ -53,6 +61,8 @@ def rcm_result_protol(some_anime_id, some_result, df):
 
     return list(unique_dict.values())
 
+    # 通过slopeone算法计算用户评分过的动画与其他相似用户的评分的差值
+    # some_protol_result 通过rcm_result_protol函数得出的结果
 def rcm_result_with_slopeone(some_protol_result,user_id,df):
     list = []
     for i in range(len(some_protol_result)):
@@ -62,9 +72,13 @@ def rcm_result_with_slopeone(some_protol_result,user_id,df):
             "predict_rating":df.loc[(df['user_id'] == user_id) & (df['anime_id'] == some_protol_result[i]["protol_anime_id"])]['rating'].tolist()[0]+anime_rating_deficiency(df,some_protol_result[i]['protol_anime_id'],some_protol_result[i]['rcm_anime_id'])
         })
     return sorted(list,key=lambda x: x['predict_rating'], reverse=True) 
-    
+
+
+    # 寻找相似用户
 def find_similiar_user(vector, df):
     print(4)
+
+    # 定义皮尔森向量计算函数
     def calculate_pearsonr(row):
         vector_or = row.fillna(0)
         pc = pearsonr(vector, vector_or)
@@ -74,14 +88,17 @@ def find_similiar_user(vector, df):
     with ThreadPoolExecutor() as executor:
         result = list(executor.map(calculate_pearsonr, [row for _, row in df.iterrows()]))
 
+    # 得出结果 转化成dataFrame 并根据相关系数降序排列
     result = pd.DataFrame(result)
     sorted_result = result.sort_values(by="statistic", ascending=False)
 
+    # 得出相关度最高的100个用户
     top_users = sorted_result.head(100)["user_id"].tolist()
 
-    # 使用向量化操作替换循环
+    # 使用向量化操作替换循环 得到相关系数最高的100个用户的用户评分矩阵
     matrix = df.loc[top_users].fillna(0).values
 
+    # 计算每一列动画的评分平均值 降序排列输出结果
     return pd.DataFrame(matrix).mean().sort_values(ascending=False).index
 
 def remove_dumplicate_element(alist,blist):
@@ -95,11 +112,20 @@ def remove_dumplicate_element(alist,blist):
 
 app = Flask(__name__)
 
+# 链接redis服务器
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+# 读入anime以及rating数据集
 anime = pd.read_csv(r"python\anime_data\anime.csv")
 rating = pd.read_csv(r"python\anime_data\rating.csv")
+
+# 删除user_id与anime_id相同的rating记录，去掉脏数据
 rating = rating.drop_duplicates(subset=['user_id', 'anime_id'])
+
+
 rating['rating'] = rating['rating'].clip(lower=0)
+
+# 使用rating表与anime表组成用户评分矩阵
 user_rating_matrix = rating.pivot(index="user_id",columns="anime_id",values="rating")
 
 @app.route('/')
@@ -108,17 +134,39 @@ def hello():
 
 @app.route('/rcm/<id>')
 def rcm(id):
+
+    # 创建空向量
     vector = pd.DataFrame(np.zeros((1,11200)),columns=user_rating_matrix.columns)
+
+    # 获取特定用户的评分记录，['user_id','anime_id','rating']
     sqls = getSql(id)
+
+    # 将空向量通过评分记录填充，获得用户评分向量
     for i in range(sqls.shape[0]):
         vector[sqls.loc[i]['anime_id']] = sqls.loc[i]['rating']
+
+    # 获取用户评分过的动画id
     rated_anime_ids = sqls['anime_id'].tolist()
+
+
     result = [item["rcm_anime_id"] for item in rcm_result_with_slopeone(rcm_result_protol(rated_anime_ids,find_similiar_user(vector.loc[0],user_rating_matrix),rating),int(id),sqls)]
+
+    # 删除redis中的缓存
     if(len(redis_client.lrange(f"result_{id}",0,-1))>0):
         redis_client.lrem(f"result_{id}",0,-1) 
+
+    # 将结果存入redis
     for item in remove_dumplicate_element(result,rated_anime_ids):
         redis_client.rpush(f"result_{id}",item)
+    
+    # 设置过期时间
     redis_client.expire(f"result_{id}",30*60)
+
+    # 输出Recall Precision F-measure 
+    # Recall = (推荐列表与用户评分过的动画id的交集)/(用户评分过的动画id的并集)
+    # Precision = (推荐列表与用户评分过的动画id的交集)/(推荐列表)
+    # F-measure = 2*Recall*Precision/(Recall+Precision)
+
     print("@Recall:")
     recall = len(set(result)&set(rated_anime_ids))/len(set(rated_anime_ids))
     print(recall)
@@ -128,6 +176,8 @@ def rcm(id):
     print("F-measure:")
     f_measure = 2*recall*precision/(recall+precision)
     print(f_measure)
+
+    # 返回推荐列表
     return json.dumps(remove_dumplicate_element(result,rated_anime_ids))
 
 
